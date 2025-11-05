9cc351b7c24bbaf8504f213bfe5f765fd2a08d50
rbm0524 <rbm0524@naver.com>
2025-11-05 21:45:32 +0900

    Handle invalid commands

--- src/main.c
+++ src/main.c
@@ -11,6 +11,9 @@ int main(int argc, char *argv[]) {
   char command[1024];
 
   fgets(command, sizeof(command), stdin);
+
+  command[strcspn(command, '\n')] = '\0';
+
   printf("%s: command not found\n", command);
 
   return 0;
