62925411587508ae2371e026a69430a8a6309c85
rbm0524 <rbm0524@naver.com>
2025-11-07 10:20:45 +0900

    implement repl

--- src/main.c
+++ src/main.c
@@ -6,16 +6,13 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
-
-  char command[1024];
-
-  fgets(command, sizeof(command), stdin);
-
-  command[strcspn(command, "\n")] = '\0';
-
-  printf("%s: command not found\n", command);
+  while (1) {
+    printf("$ ");
+    char command[1024];
+    fgets(command, sizeof(command), stdin);
+    command[strcspn(command, "\n")] = '\0';
+    printf("%s: command not found\n", command);
+  }
 
   return 0;
 }
