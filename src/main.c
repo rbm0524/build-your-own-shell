54c1db6aa9a9e9780747a667352419549ce62e52
rbm0524 <rbm0524@naver.com>
2025-11-07 10:38:09 +0900

    Implement exit

--- src/main.c
+++ src/main.c
@@ -11,6 +11,15 @@ int main(int argc, char *argv[]) {
     char command[1024];
     fgets(command, sizeof(command), stdin);
     command[strcspn(command, "\n")] = '\0';
+
+    if(strcmp(command, "exit")) {
+      if(command[strcspn(command, "\0") - 1] == '0') {
+        exit(0);
+      } else {
+        exit(1);
+      }
+    }
+
     printf("%s: command not found\n", command);
   }
 
