1027d2cd0b4f62bf285ff7a4e23bab3bc6b0cf80
rbm0524 <rbm0524@naver.com>
2025-11-17 23:32:37 +0900

    feat: 명령어 목록과 함수 이름 헤더 파일로 분리

---
+++ src/builtin.h
@@ -0,0 +1,28 @@
+char *commandTable[] = {"exit", "echo", "type"};
+
+void builtin_exit(char *args) {
+  if (args != NULL) {
+    int exit_code = atoi(args);
+    exit(exit_code);
+  } else {
+    exit(0);
+  }
+}
+
+void builtin_echo(char *args) {
+  if (args != NULL) {
+    printf("%s\n", args);
+  } else {
+    printf("\n");
+  }
+}
+
+void builtin_type(char *args) {
+    for(int i = 0; i < sizeof(commandTable)/sizeof(char*); i++) {
+        if(strcmp(args, commandTable[i])) {
+            printf(".");
+            return;
+        }
+    }
+    printf("<%s>: not found\n", args);
+}
