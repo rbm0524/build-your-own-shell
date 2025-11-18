f1e217515bf47eb112fbaf424dc6e99fe9a9cf1f
rbm0524 <rbm0524@naver.com>
2025-11-18 12:40:19 +0900

    feat: 환경변수 PATH 추가

--- src/main.c
+++ src/main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
 #include "builtin.h"
 
 void builtin_exit(char *args);
 void builtin_echo(char *args);
-void builtin_type(char *args);
+void builtin_type(char *args, char* envPath);
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
+  char * envPath = getenv("PATH");
 
   while (1) {
     printf("$ ");
@@ -18,7 +20,7 @@ int main(int argc, char *argv[]) {
     command[strcspn(command, "\n")] = '\0';
 
     char *cmd = strtok(command, " ");
-    char *args = strtok(NULL, "\0"); // 나머지 전체를 인수로 취급
+    char *args = strtok(NULL, "\0"); // 나머지 전체를 인자로 취급
 
     // 명령어 해석하기
     if(strstr(cmd, "exit") != NULL) { // 문자열이 서로 같으면 0을 리턴한다.
@@ -26,7 +28,7 @@ int main(int argc, char *argv[]) {
     } else if (strstr(cmd, "echo") != NULL) {
       builtin_echo(args);
     } else if(strstr(cmd, "type") != NULL) {
-      builtin_type(args);
+      builtin_type(envPath, args);
     } else {
       printf("%s: command not found\n", cmd);
     }
